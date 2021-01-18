37fff1bc0fa33f03a48bbf0863fd577c2339a0f7
Hugh Jeremy <hugh.jeremy@gmail.com>
2021-01-19 10:55:19 +1100

    add missing term rate inits

--- procuret/__init__.py
+++ procuret/__init__.py
@@ -6,3 +6,5 @@ from procuret.data.order import Order
 from procuret.ancillary.entity_headline import EntityHeadline
 from procuret import errors
 from procuret.version import VERSION
+from procuret.term_rate.term_rate import TermRate
+from procuret.term_rate.group import TermRateGroup
